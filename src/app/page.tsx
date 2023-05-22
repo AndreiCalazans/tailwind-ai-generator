f91e395b1319333243a6c010dd64913265fce965
Andrei Calazans <andreixoc@hotmail.com>
2023-05-22 15:06:06 -0300

    format!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
-import CodePlayground from './coms/CodePlayground';
-import Prompt from './coms/Prompt';
-import Link from 'next/link';
+import CodePlayground from "./coms/CodePlayground";
+import Prompt from "./coms/Prompt";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-evenly gap-6">
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Tailwind Component Generator</span>
-          <span className="block text-2xl text-indigo-600"><Link className="underline" href="/gallery">Copy from gallery</Link> or generate from prompt ↓</span>
-        </h2>
+        <span className="block">Tailwind Component Generator</span>
+        <span className="block text-2xl text-indigo-600">
+          <Link className="underline" href="/gallery">
+            Copy from gallery
+          </Link>{" "}
+          or generate from prompt ↓
+        </span>
+      </h2>
       <Prompt />
       <CodePlayground />
     </main>
-  )
+  );
 }
